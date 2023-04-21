0d1c5bdb0c0f0fdd07cbbf9c1fe6433a20d08b33
DLeves <dittrichlevi@gmail.com>
2023-04-21 11:19:22 +0200

    base plot

--- csoportmunka.R
+++ csoportmunka.R
@@ -1,5 +1,11 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(quantmod)
+library(tidyverse)
 
-eur = getSymbols("EURHUF=X", src = "yahoo", from = "2021-01-01", to = "2022-12-31", auto.assign = F)
+eurxts = getSymbols("EURHUF=X", src = "yahoo", from = "2021-01-01", to = "2022-12-31", auto.assign = F)
 
+eur = data.frame(time = index(eurxts), eurhuf = as.numeric(eurxts$`EURHUF=X.High`))
+
+ggplot(eur, aes(x = time))+
+  geom_line(aes(y = eurhuf, color = "EUR/HUF árfolyam"))+
+  theme_minimal()
